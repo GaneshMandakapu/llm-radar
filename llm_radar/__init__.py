e61ded6a4f7e3f26837d849e548d4da69870966c
GaneshBalaraju <g.balaraju@steinbeis-next.de>
2026-05-04 11:25:50 +0200

    chore: bump version to v0.3.0

--- llm_radar/__init__.py
+++ llm_radar/__init__.py
@@ -2,5 +2,5 @@ from .core import LLMRadar
 from .plugins.fastapi_radar import LLMRadarPlugin
 from .ab_testing import ABTestResult, VariantResult
 
-__version__ = "0.2.0"
+__version__ = "0.3.0"
 __all__ = ["LLMRadar", "LLMRadarPlugin", "ABTestResult", "VariantResult"]
